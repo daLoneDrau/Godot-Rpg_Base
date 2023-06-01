e53298d4b2c707b43d9d2d48c58567bef0c36ce2
daLoneDrau <daLoneDrau@users.noreply.github.com>
2023-06-01 10:26:54 -0400

    Update NesMainSceneContainer.cs

--- Scenes/Controllers/NesMainSceneContainer.cs
+++ Scenes/Controllers/NesMainSceneContainer.cs
@@ -6,18 +6,6 @@ namespace Base.Scenes.Controllers
     public abstract class NesMainSceneContainer : NesController
     {
         /// <summary>
-        /// Called when the node enters the scene tree for the first time.
-        /// </summary>
-        public override void _Ready()
-        {
-            base._Ready();
-            ConsumeStateEvent();
-        }
-        /// <summary>
-        /// Consumes any signals of a state change event.
-        /// </summary>
-        public abstract void ConsumeStateEvent();
-        /// <summary>
         /// Hides all widgets.
         /// </summary>
         protected abstract void HideAll();
